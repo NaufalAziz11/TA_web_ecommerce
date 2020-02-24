18ac7db094aff0e95034cda5ee8a86f423a4a2d6
NaufalAziz11 <delnaufal11@gmail.com>
2020-02-25 00:15:44 +0700

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -45,6 +45,24 @@
 	<title><?= $judul; ?></title>
 
 </head>
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Logout</h5>
+				<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">×</span>
+				</button>
+			</div>
+			<div class="modal-body">Yakin ingin keluar dari akun ?</div>
+			<div class="modal-footer">
+				<button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
+				<a class="btn btn-primary" href="<?= base_url('login/logout'); ?>">Logout</a>
+			</div>
+		</div>
+	</div>
+</div>
 
 <body>
 	<div class="fixed-top">
